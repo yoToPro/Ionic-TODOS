0c3ef41c9829b9330b2fc7130381ff9afcc4bd41
yoToPro <kd.dhiru007@gmail.com>
2019-11-08 02:48:31 +0530

    CRUD completed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { AlertController, IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  public todoItem = '';
+  @ViewChild('todoName', null) todoInput: IonInput;
+
+  public todo = '';
   public todoList = [];
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController
+  ) { }
 
-  addTodoItem() {
-    if (this.todoItem.length > 0) {
-      this.todoList.push(this.todoItem);
-      this.todoItem = '';
+  ionViewDidEnter() {
+    setTimeout(() => {
+      this.todoInput.setFocus();
+    }, 350);
+  }
+
+  addTodo() {
+    if (this.todo.length > 0) {
+      this.todoList.push(this.todo);
+      this.todo = '';
+      this.todoInput.setFocus();
     }
+  }
+
+  async updateTodo(index: number, task: string) {
+    const updateAlert = await this.alertCtrl.create({
+      header: 'Update Task',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTodo', placeholder: 'Task' }],
+      buttons: [
+        { text: 'Cancel', role: 'Cancel' },
+        {
+          text: 'Update',
+          handler: (data: any | null) => {
+            if (data.editTodo.length > 0) {
+              this.todoList[index] = data.editTodo;
+            } else {
+              this.todoList[index] = task;
+            }
+          }
+        }
+      ]
+    });
+    await updateAlert.present();
+  }
+
+  deleteTodo(index: number) {
+    this.todoList.splice(index, 1);
   }
 }
